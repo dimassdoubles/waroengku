a24c343068b8a6219084c639587f2723d83c5bd3
Dimas Saputro <111202012497@mhs.dinus.ac.id>
2022-12-07 11:59:12 +0700

    berhasil hapus cart

--- lib/presentation/widgets/user_widgets/cart_item.dart
+++ lib/presentation/widgets/user_widgets/cart_item.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:waroengku/domain/entity/cart.dart';
+import 'package:waroengku/injection_container.dart';
+import 'package:waroengku/presentation/blocs/auth/auth_bloc.dart';
+import 'package:waroengku/presentation/blocs/auth/auth_state.dart';
+import 'package:waroengku/presentation/blocs/cart/cart_bloc.dart';
+import 'package:waroengku/presentation/blocs/cart/cart_event.dart';
+import 'package:waroengku/share/routes.dart';
 import 'package:waroengku/share/styles/colors.dart';
 
 class CartItem extends StatelessWidget {
@@ -11,98 +18,134 @@ class CartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 16,
-        vertical: 4,
-      ),
-      child: Container(
-        padding: EdgeInsets.all(8),
-        width: double.infinity,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5),
-          color: Colors.white,
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 3,
-              offset: Offset(0.7, 1),
-            ),
-          ],
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, detailPage, arguments: cart.product);
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 4,
         ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width / 3,
-              decoration: BoxDecoration(
-                color: Colors.grey[100],
-                borderRadius: BorderRadius.circular(5),
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: Colors.white,
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 3,
+                offset: Offset(0.7, 1),
               ),
-              child: AspectRatio(
-                aspectRatio: 1,
-                child: SizedBox(
-                  width: double.infinity,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(5),
-                    child: Image.network(cart.product.image),
+            ],
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width / 3,
+                decoration: BoxDecoration(
+                  color: Colors.grey[100],
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(5),
+                      child: Image.network(cart.product.image),
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(
-              width: 8,
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    cart.product.name,
-                    maxLines: 1,
-                    style: const TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                  Text(
-                    "Rp ${cart.product.price}",
-                    style: const TextStyle(
-                      color: kPrimaryColor,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 16,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  Text(
-                    "Qty: ${cart.quantity}",
-                    style: const TextStyle(
-                      color: Colors.grey,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  Container(
-                    color: kPrimaryColor,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 8,
-                    ),
-                    child: const Text(
-                      "Hapus",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+              const SizedBox(
+                width: 8,
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      cart.product.name,
+                      maxLines: 1,
+                      style: const TextStyle(
+                        fontSize: 16,
                       ),
                     ),
-                  ),
-                ],
+                    Text(
+                      "Rp ${cart.product.price}",
+                      style: const TextStyle(
+                        color: kPrimaryColor,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      "Qty: ${cart.quantity}",
+                      style: const TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    BlocBuilder(
+                      bloc: getIt<AuthBloc>(),
+                      builder: (context, authState) {
+                        if (authState is Authenticated) {
+                          return InkWell(
+                            onTap: () {
+                              getIt<CartBloc>().add(
+                                CartDelete(
+                                  token: authState.user.token,
+                                  id: cart.id,
+                                ),
+                              );
+                            },
+                            child: Container(
+                              color: kPrimaryColor,
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 16,
+                                vertical: 8,
+                              ),
+                              child: const Text(
+                                "Hapus",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          );
+                        }
+                        return Container(
+                          color: Colors.grey,
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 8,
+                          ),
+                          child: const Text(
+                            "Hapus",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
